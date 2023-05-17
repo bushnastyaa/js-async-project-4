2f841021b51aafd550524846a7975f9bb5d87a13
Anastasia Bushmakina <bushnastyaa@mail.ru>
2023-05-17 17:49:41 +0300

    Update commander

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -1,19 +1,20 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import { readFileSync } from 'fs';
+import { createRequire } from 'module';
 import pageLoader from '../src/index.js';
 
-const { version } = JSON.parse(readFileSync(new URL('../package.json', import.meta.url)));
+const require = createRequire(import.meta.url)
+const { version } = require('../package.json');
 
 const program = new Command();
 
 program
   .version(version, '-V, --version', 'output the version number')
   .description('Page-loader utility')
-  .option('-o, --output [dir]', 'output dir (default: "/home/user/current-dir")', process.cwd())
+  .option('-o, --output [dir]', 'output dir', process.cwd())
   .arguments('<url>')
   .action((url, { output }) => pageLoader(url, output)
-    .then((filePath) => console.log(filePath))
+    .then((filePath) => console.log(`File was successfully downloaded into '${filePath}'`))
     .catch((error) => {
       console.error(error.message);
       process.exit(1);
