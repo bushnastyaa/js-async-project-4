99db36f53f2730fe0851c3758a20b94bc10664cb
Anastasia Bushmakina <bushnastyaa@mail.ru>
2023-05-18 12:49:45 +0300

    Add debuger

--- src/index.js
+++ src/index.js
@@ -3,7 +3,23 @@ import path from 'path';
 import { cwd } from 'node:process';
 import axios from 'axios';
 import Listr from 'listr';
+import debug from 'debug';
+import axiosdebug from 'axios-debug-log';
 import { downloadAssets, replaceName, loadingLinks } from './utils.js';
+
+const logger = debug('page-loader');
+
+axiosdebug({
+  response(deb, response) {
+    debug(
+      `Response with ${response.headers['content-type']}`,
+      `from ${response.config.url}`,
+    );
+  },
+  error(deb, error) {
+    debug('Error: ', error);
+  },
+});
 
 export default (url, output = cwd()) => {
   const fileName = replaceName(url);
@@ -13,12 +29,17 @@ export default (url, output = cwd()) => {
   const { origin } = new URL(url);
 
   return axios.get(url)
-    .then((response) => fsp.mkdir(outPath)
-      .then(() => (response.data)))
+    .then((response) => {
+      logger(`Сreating directory: ${assetsDir.toString()}`);
+
+      return fsp.mkdir(outPath)
+        .then(() => (response.data));
+    })
     .then((response) => {
       const { html, assets } = loadingLinks(response, assetsDir, origin);
       const tasks = assets.map(([assetsUrl, assetsFilePath]) => {
         const { href } = new URL(assetsUrl, origin);
+        logger(`Downloading file ${href}`);
         return {
           title: href,
           task: () => downloadAssets(href, path.join(output, assetsFilePath)),
